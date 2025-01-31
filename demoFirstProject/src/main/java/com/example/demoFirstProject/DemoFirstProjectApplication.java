5141235c23eba7d349142e5cac035d82ef8e2a2a
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-01 00:33:21 +0530

    first springboot commit

---
+++ demoFirstProject/src/main/java/com/example/demoFirstProject/DemoFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoFirstProjectApplication.class, args);
+	}
+
+}
